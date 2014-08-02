a428908a6dd88f351775d1d3da530a1a2aa0d6cb
José Valim <jose.valim@plataformatec.com.br>
2014-08-02 22:09:37 +0200

    Relax Elixir dependency

--- mix.exs
+++ mix.exs
@@ -18,11 +18,11 @@ defmodule Markdown.Mixfile do
   @version String.strip(File.read!("VERSION"))
 
   def project do
-    [ app: :markdown,
-      version: @version,
-      elixir: "~> 0.14.3",
-      compilers: [:hoedown, :elixir, :app],
-      deps: deps ]
+    [app: :markdown,
+     version: @version,
+     elixir: ">= 0.14.3 and < 2.0.0",
+     compilers: [:hoedown, :elixir, :app],
+     deps: deps]
   end
 
   # Configuration for the OTP application
@@ -33,6 +33,6 @@ defmodule Markdown.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :hoedown, github: "hoedown/hoedown", app: false } ]
+    [{:hoedown, github: "hoedown/hoedown", app: false}]
   end
 end
