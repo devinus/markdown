38dc74c88d52fd7569ad4f63d32b344d35a7fcce
José Valim <jose.valim@gmail.com>
2016-10-10 10:24:48 +0200

    Avoid warnings on recent Elixir versions

--- mix.exs
+++ mix.exs
@@ -24,16 +24,13 @@ defmodule Markdown.Mixfile do
      version: @version,
      elixir: ">= 0.14.3 and < 2.0.0",
      compilers: [:hoedown, :elixir, :app],
-     deps: deps]
+     deps: deps()]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [{:hoedown, github: "hoedown/hoedown", app: false}]
   end
